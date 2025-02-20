65a05add5d6b751a2333df4ba1310cba319b7ee1
Williams Medina <90166309+williamsmedina@users.noreply.github.com>
2025-02-20 14:39:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,13 +53,13 @@ resource "azurerm_network_security_group" "nsg" {
   
 
   security_rule {
-    name                       = "Allow-Port-23"
+    name                       = "Allow-Port-22"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "23"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
     
