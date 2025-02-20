779f94960814deacb3952b160438ce9fd67d445c
Williams Medina <90166309+williamsmedina@users.noreply.github.com>
2025-02-20 11:14:15 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,129 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "name" {
+  description = "Nombre base para la creación de recursos."
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Nombre de usuario administrador para la VM."
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Contraseña del administrador para la VM."
+  type        = string
+  sensitive   = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.name}"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vn-${var.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["192.168.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["192.168.100.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${var.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+
+  security_rule {
+    name                       = "Allow-Port-23"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "23"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    
+  }
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-${var.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${var.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-${var.name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+output "resource_group" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "vm_name" {
+  value = azurerm_linux_virtual_machine.vm.name
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
